5adcc34b57e16149107c53e07a16466edd4ade3e
Matt Tierney <tierneybbq@gmail.com>
2022-12-18 00:40:27 -0600

    navbar linked

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Navbar />
     </div>
-  )
-}
+  );
+};
 
 export default Home
+
+
