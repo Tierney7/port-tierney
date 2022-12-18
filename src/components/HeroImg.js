079a1dad79189c1582119ddca0d618b95a94e836
Matt Tierney <tierneybbq@gmail.com>
2022-12-18 13:45:57 -0600

    hero css and buttons

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -3,13 +3,24 @@ import "./HeroImgStyles.css";
 import React from 'react'
 
 import IntroImg from "../assets/header-background.jpg"
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
-  return <div className="hero">
+  return (
+     <div className="hero">
       <div className="mask">
         <img className="intro-img" src={IntroImg} alt="IntroImg" />
       </div>
-    </div>;
+      <div className="content">
+        <p>Matt Tierney</p>
+        <h1>Full-Stack Developer</h1>
+        <div>
+            <Link to="/project" className="btn">Projects</Link>
+            <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
   };
 
 export default HeroImg;
