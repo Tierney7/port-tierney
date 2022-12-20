bd51e0dbe694c48c9c38d16ec51f7249e8f88b86
Matt Tierney <tierneybbq@gmail.com>
2022-12-19 19:02:41 -0600

    orange matching homepage

--- src/components/HeroImg2.js
+++ src/components/HeroImg2.js
@@ -6,12 +6,12 @@ class heroImg2 extends Component {
     render() {
   return (
     <div className="hero-img">
-        <div className="heading">
-    <h1>{this.props.heading}</h1>
+      <div className="heading">
+        <h1>{this.props.heading}</h1>
     <p>{this.props.text}</p>
         </div>
         </div>
-  );
+      );
 }
 }
 export default heroImg2
