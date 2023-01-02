3536ff98fb14456b48a61a7453b9cf046910cb3f
Matt Tierney <tierneybbq@gmail.com>
2023-01-01 18:13:26 -0600

    final copy for now

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import "./FooterStyles.css"
 import React from 'react'
-import { FaHome, FaPhone, FaMailBulk, FaGithub, FaLinkedin, FaInstagram, } from "react-icons/fa";
+import { FaHome, FaPhone, FaMailBulk, FaGithub, FaLinkedin, FaInstagram, FaBookOpen, } from "react-icons/fa";
 
 
 const footer = () => {
@@ -22,16 +22,28 @@ const footer = () => {
         <div className="email">
             <h4><FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }}/>tierneycode@gmail.com</h4>
         </div>
+        <div className="resume">
+          <a href="https://docs.google.com/document/d/1EQIT-5I_wqwy8EERw-OWgSn1mOfJyc2RZ--1mjKnqv0/edit?usp=sharing">
+            <h4><FaBookOpen size={20} style={{ color: "#fff", marginRight: "2rem" }}/>View Resume</h4>
+        </a>
         </div>
+        </div>
+        
+
 
         <div className="right">
             <h4>Matt Tierney</h4>
             <p>Full-Stack Web Developer, and University of Kansas Bootcamp graduate based in Kansas City, Missouri.</p>
             <div className="social">
-                   
+            <a href="https://github.com/Tierney7">  
             <FaGithub size={30} style={{ color: "#fff", marginRight: "1rem" }}/>
+            </a>
+            <a href="https://www.linkedin.com/in/tierneycode/">
             <FaLinkedin size={30} style={{ color: "#fff", marginRight: "1rem" }}/>
+            </a>
+            <a href="https://www.instagram.com/tierneyssmokedgoods/">
             <FaInstagram size={30} style={{ color: "#fff", marginRight: "1rem" }}/>
+            </a>
             </div>
         </div>
       </div>
