89804d35edc1f0e21d284e2d69a1304c8b3825c1
Matt Tierney <tierneybbq@gmail.com>
2023-01-01 18:36:14 -0600

    boom:final:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,22 +9,24 @@ const footer = () => {
       <div className="footer-container">
         <div className="left">
             <div className="location">
-            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }}/>
+            <FaHome size={20} style={{ color: "rgba(134, 98, 1)", marginRight: "2rem" }}/>
             <div>
                 <p>Kansas City, Missouri</p>
                 <p>United States</p>
             </div>
             </div>
             <div className="phone">
-            <h4><FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }}/>
+            <h4><FaPhone size={20} style={{ color: "rgba(134, 98, 1)", marginRight: "2rem" }}/>
             (816) 914-0503</h4>
             </div>
         <div className="email">
-            <h4><FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }}/>tierneycode@gmail.com</h4>
+          <a href="mailto:tierneycode@gmail.com">
+            <h4><FaMailBulk size={20} style={{ color: "rgba(134, 98, 1)", marginRight: "2rem" }}/>tierneycode@gmail.com</h4>
+        </a>
         </div>
         <div className="resume">
           <a href="https://docs.google.com/document/d/1EQIT-5I_wqwy8EERw-OWgSn1mOfJyc2RZ--1mjKnqv0/edit?usp=sharing">
-            <h4><FaBookOpen size={20} style={{ color: "#fff", marginRight: "2rem" }}/>View Resume</h4>
+            <h4><FaBookOpen size={20} style={{ color: "rgba(134, 98, 1)", marginRight: "2rem" }}/>View Resume</h4>
         </a>
         </div>
         </div>
@@ -36,13 +38,13 @@ const footer = () => {
             <p>Full-Stack Web Developer, and University of Kansas Bootcamp graduate based in Kansas City, Missouri.</p>
             <div className="social">
             <a href="https://github.com/Tierney7">  
-            <FaGithub size={30} style={{ color: "#fff", marginRight: "1rem" }}/>
+            <FaGithub size={30} style={{ color: "rgba(134, 98, 1)", marginRight: "1rem" }}/>
             </a>
             <a href="https://www.linkedin.com/in/tierneycode/">
-            <FaLinkedin size={30} style={{ color: "#fff", marginRight: "1rem" }}/>
+            <FaLinkedin size={30} style={{ color: "rgba(134, 98, 1)", marginRight: "1rem" }}/>
             </a>
             <a href="https://www.instagram.com/tierneyssmokedgoods/">
-            <FaInstagram size={30} style={{ color: "#fff", marginRight: "1rem" }}/>
+            <FaInstagram size={30} style={{ color: "rgba(134, 98, 1)", marginRight: "1rem" }}/>
             </a>
             </div>
         </div>
